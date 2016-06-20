314201f056e7f4e5b394be314b707983603e0229
gitolite <EFox2413@gmail.com>
2016-06-20 13:44:44 -0700

    fixed image linking

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/main'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 root 'welcome#index'
+get 'main.css' => 'welcome#main'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
