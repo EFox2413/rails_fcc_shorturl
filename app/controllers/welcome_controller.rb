137b0c5792174aebd8ca48393ea8d87ee2b2290d
gitolite <EFox2413@gmail.com>
2016-06-22 16:07:40 -0700

    Added some string processing to Welcome#getheader function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -25,6 +25,9 @@ class WelcomeController < ApplicationController
         lang = request.env["HTTP_ACCEPT_LANGUAGE"]
         software = request.env["HTTP_USER_AGENT"]
 
+        lang = lang[/(.*),/, 1]
+        software = software[/\((.*)\)/, 1].split(')')[0]
+
         render json: { ipaddress: ip, language: lang, software: software}
     end
 end
